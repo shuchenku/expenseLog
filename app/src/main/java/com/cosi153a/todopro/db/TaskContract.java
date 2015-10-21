96dcaf412b07023835975f3cc01eb045c8cb3add
SHU CHEN <chenshu8805@gmail.com>
2015-10-21 02:30:30 -0400

    first commit

---
+++ app/src/main/java/com/cosi153a/todopro/db/TaskContract.java
@@ -0,0 +1,17 @@
+package com.cosi153a.todopro.db;
+
+/**
+ * Created by apple on 10/21/15.
+ */
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.cosi153.todoapp.db";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;
+    }
+}
